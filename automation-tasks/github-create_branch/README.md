ec12097cd2125d753a8802bd76e138e320e2ec26
Rajesh Deshpande <47311048+rajeshdeshpande02@users.noreply.github.com>
2025-04-28 02:19:14 +0000

    automation to create github branch

---
+++ automation-tasks/github-create_branch/README.md
@@ -0,0 +1,18 @@
+## What it does 
+
+This automation helps to create a github branch from a specific branch. This can be used in platform for auto branch creation or default branch setup along with repo setup
+
+## How to execute it?
+
+Run following command:
+
+````sh
+ $ go run create_branch.go --repo-name=rajeshdeshpande02/test-repo  --base-branch develop --new-branch test
+
+```
+
+Pre-requisite:
+
+````sh
+export GHUB_TOKEN = "<Github token with appropriate access>"
+````
--- automation-tasks/github-delete_repo/main.go
+++ automation-tasks/github-delete_repo/main.go
@@ -1,54 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-const (
-	url = "https://api.github.com/user/repos"
-)
+type GitHubClient struct {
+	HTTPClient *http.Client
+	Token      string
+}
 
-func main() {
-
-	github_token := os.Getenv("GHUB_TOKEN")
-
-	if github_token == "" {
-		fmt.Println("Error getting GHUB_TOKEN from environment variables")
-		return
-	}
-	
-	repoName := "rajeshdeshpande02/test-repo"
+func (c *GitHubClient) DeleteRepo(repoName string) error {
 	url := fmt.Sprintf("https://api.github.com/repos/%s", repoName)
-	fmt.Println("URL:", url)
-
-	// Create a new HTTP client
-	httpClient := &http.Client{}
-
-	// Create a new request
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		return fmt.Errorf("error creating request: %w", err)
 	}
 
-	// Set the request headers
 	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "Bearer " + github_token)
+	req.Header.Set("Authorization", "Bearer "+c.Token)
 	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
 
-	//send the request
-	resp, err := httpClient.Do(req)
+	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		fmt.Println("Error sending delete request:", err)
-		return
+		return fmt.Errorf("error sending delete request: %w", err)
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusNoContent {
-		fmt.Println("Error creating repository:", resp.Status)
-		return
-	}
-	fmt.Println("Repository delete successfully: ", repoName)
 
+	if resp.StatusCode != http.StatusNoContent {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("error deleting repository: %s, response: %s", resp.Status, string(body))
+	}
+
+	return nil
+}
+
+func main() {
+	githubToken := os.Getenv("GHUB_TOKEN")
+	if githubToken == "" {
+		log.Fatal("Error: GHUB_TOKEN environment variable is not set")
+	}
+
+	if len(os.Args) < 2 {
+		log.Fatal("Error: Repository name argument is required")
+	}
+
+	repoName := os.Args[1]
+	client := &GitHubClient{
+		HTTPClient: &http.Client{},
+		Token:      githubToken,
+	}
+
+	err := client.DeleteRepo(repoName)
+	if err != nil {
+		log.Fatalf("Failed to delete repository: %v", err)
+	}
+
+	fmt.Println("Repository deleted successfully:", repoName)
 }
